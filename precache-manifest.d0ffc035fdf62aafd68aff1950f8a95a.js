af4f50547e1b254bd63c169df78369cd5ccca602
Akanksha <karnakanksha24@gmail.com>
2023-05-02 23:49:03 +0530

    Updates

---
+++ precache-manifest.d0ffc035fdf62aafd68aff1950f8a95a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "39315b35a9c37cc5745c",
+    "url": "/my-todo-app/static/css/main.be1373e5.chunk.css"
+  },
+  {
+    "revision": "39315b35a9c37cc5745c",
+    "url": "/my-todo-app/static/js/main.39315b35.chunk.js"
+  },
+  {
+    "revision": "df667a71932a219e4844",
+    "url": "/my-todo-app/static/js/1.df667a71.chunk.js"
+  },
+  {
+    "revision": "1d94776efbcd5692b0ef",
+    "url": "/my-todo-app/static/js/runtime~main.1d94776e.js"
+  },
+  {
+    "revision": "eb3150bdc845053bef14802b88d92574",
+    "url": "/my-todo-app/static/media/app-icon.eb3150bd.png"
+  },
+  {
+    "revision": "a3e432f2c397cd6f89db08d69d7026ec",
+    "url": "/my-todo-app/static/media/bg.a3e432f2.jpg"
+  },
+  {
+    "revision": "92490f72c874919541b433c5a8a9341b",
+    "url": "/my-todo-app/index.html"
+  }
+];
